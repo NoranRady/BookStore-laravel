4d2ceac88c0d1a7fe62289a1a32ae916ca3d3dcb
NoranRady <noran.rady023@gmail.com>
2019-08-15 18:14:57 +0200

    Refactore

--- app/Notifications/WelcomeMail.php
+++ app/Notifications/WelcomeMail.php
@@ -7,15 +7,10 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class WelcomeMail extends Notification
+class WelcomeMail extends Notification implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
     public $user;
     public function __construct($user)
     {
@@ -23,23 +18,11 @@ class WelcomeMail extends Notification
         $this->user = $user;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
     public function toMail($notifiable)
     {
         return (new MailMessage)
@@ -50,12 +33,6 @@ class WelcomeMail extends Notification
                     ->markdown('welcome.index', ['user' => $this->user->name]);
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function toArray($notifiable)
     {
         return [
